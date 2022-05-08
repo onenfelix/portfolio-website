1755cf2873026f1986f0ca40b5995a88d391560b
Felix Onen <onenfelix90@gmail.com>
2022-05-09 02:51:59 +0300

    Portfolio

---
+++ constants/data.js
@@ -0,0 +1,105 @@
+const userData = {
+  githubUsername: "onenfelix",
+  name: "Felix Onen",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "onenfelix90@gmail.com",
+  phone: "+256 785450481",
+  address: "Kampala, Uganda.",
+  projects: [
+    {
+      title: "Crown Clothing Ecommerce",
+      link: "https://crownclothing.vercel.app/",
+      imgUrl: "/crown.png",
+    },
+    {
+      title: "Estates Booking",
+      link: "http://estatesbooking.herokuapp.com/",
+      imgUrl: "/estates.png",
+    },
+    {
+      title: "Election Analysis",
+      link: "https://mark-okello.vercel.app/",
+      imgUrl: "/election-analysis.png",
+    },
+    {
+      title: "Electronic Voting",
+      link: "https://www.evoting.busitema.ac.ug/",
+      imgUrl: "/evoting.png",
+    },
+    {
+      title: "Examination Portal",
+      link: "https://exams.busitema.ac.ug/",
+      imgUrl: "/exams.png",
+    },
+    {
+      title: "Job Board",
+      link: "https://codemaster.pythonanywhere.com/",
+      imgUrl: "/jobboard.png",
+    }
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      `I've been developing full-stack application since I was in the university. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2022 now.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I'm a legend. I've been coding in React, Node.js, Express, Typecript,  MongoDB, MySQL, PostgreSQL, Bootstrap, Tailwind, Python, Django, Golang, Solidity and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "System Status Monitor",
+    currentProjectUrl: "https://github.com/onenfelix/uptime-monitor",
+  },
+  experience: [
+    {
+      title: "Software Developer",
+      company: "Medx International",
+      year: "2021",
+      companyLink: "https://medx.international/",
+      desc: "Contributed to our Labxpert product - which is an end-to-end laboratory information system to manage the entire medical testing process .",
+    },
+    {
+      title: "Backend Engineer Intern",
+      company: "Cardiff Networks",
+      year: "2021",
+      companyLink: "https://#",
+      desc: "Develop Enterprise Systems for corporate clients",
+    },
+    {
+      title: "IT Support Specialist",
+      company: "Technology Dynasty",
+      year: "2021",
+      companyLink: "https://https://technologydynasty.com/",
+      desc: "Provide IT support services and software product support to clients",
+    },
+    {
+      title: "Intern Software Engineer",
+      company: "Hackshade Technologies",
+      year: "2018",
+      companyLink: "https://#",
+      desc: "Developed and maintain web systems for clients",
+    },
+    {
+      title: "Nanodegree",
+      company: "Udacity",
+      year: "2019",
+      companyLink: "https://www.udacity.com/",
+      desc: "Nanodegree in AI programming with python",
+    },
+    {
+      title: "Graduation",
+      company: "Busitema University",
+      year: "2015",
+      companyLink: "https://busitema.ac.ug/",
+      desc: "Bachelor's of Computer Engineering",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/18ZJSpyfIRuMOT3LiR2FthGKnOGsk_p1t/view?usp=sharing",
+  socialLinks: {
+    twitter: "https://twitter.com/onenfelix90",
+    linkedin: "https://www.linkedin.com/in/felixonen/",
+    github: "https://github.com/onenfelix",
+  },
+};
+
+export default userData;
